fc8e445aa7d7e9d4d586bdd12bdcebe4fcf5ca41
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-24 19:48:56 -0500

    added the model class

---
+++ src/main/java/ca/tetervak/kittymail/domain/Envelope.java
@@ -0,0 +1,28 @@
+package ca.tetervak.kittymail.domain;
+
+import java.io.Serializable;
+
+public class Envelope implements Serializable {
+
+    private int message = 1;
+    private boolean registered = false;
+
+    public Envelope() {
+    }
+
+    public int getMessage() {
+        return message;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
+
+    public boolean isRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(boolean registered) {
+        this.registered = registered;
+    }
+}

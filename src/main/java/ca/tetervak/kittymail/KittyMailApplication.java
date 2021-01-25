846a9a48b713aa177ef4ac90d0bb5f983bbaebd0
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-24 19:37:47 -0500

    made the input page

---
+++ src/main/java/ca/tetervak/kittymail/KittyMailApplication.java
@@ -0,0 +1,13 @@
+package ca.tetervak.kittymail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KittyMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tetervak/kittymail/KittyMailApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetervak.kittymail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittyMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

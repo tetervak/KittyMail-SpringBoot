8fd146fae7650cac2b8204402448ee3dc96d1124
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-01-20 21:13:08 -0500

    removed input validation

--- src/main/java/ca/tetervak/kittymail/controller/KittyMailController.java
+++ src/main/java/ca/tetervak/kittymail/controller/KittyMailController.java
@@ -4,8 +4,6 @@ import ca.tetervak.kittymail.domain.Envelope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,14 +21,9 @@ public class KittyMailController {
     }
 
     @GetMapping("/Process")
-    public ModelAndView process(
-            @Validated @ModelAttribute Envelope envelope,
-            BindingResult bindingResult){
+    public ModelAndView process(@ModelAttribute Envelope envelope){
         logger.trace("process() is called");
         logger.debug("envelope = " + envelope);
-        if(bindingResult.hasErrors()){
-            return new ModelAndView("Input");
-        }
         return new ModelAndView("Output", "envelope", envelope);
     }
 }
